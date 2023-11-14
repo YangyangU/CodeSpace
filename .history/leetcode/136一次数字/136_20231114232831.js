c8853c3334abea889bdb4405532fc51d7b26ca0d
Yangyang_U <495541711@qq.com>
2023-11-14 23:35:37 +0800

    'leetcode'

---
+++ .history/leetcode/136一次数字/136_20231114232831.js
@@ -0,0 +1,8 @@
+nums = [4,1,2,1,2]
+
+
+var singleNumber = function(nums) {
+
+};
+
+console.log(singleNumber(nums));
---
+++ .history/leetcode/136一次数字/136_20231114233014.js
@@ -0,0 +1,13 @@
+nums = [4,1,2,1,2]
+
+
+var singleNumber = function(nums) {
+    for(var i=0; i<nums.length; i++){
+        if (nums.indexOf(nums[i])===nums.lastIndexOf(nums[i])){
+            return nums[i];
+        }
+
+    }
+};
+
+console.log(singleNumber(nums));
