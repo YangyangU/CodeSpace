09888adf0c7699da33734f9f7073e9ca18734d83
Yangyang_U <495541711@qq.com>
2023-11-17 11:05:33 +0800

    '小程序3'

--- waibao/dreamcar/pages/index/index.js
+++ waibao/dreamcar/pages/index/index.js
@@ -7,9 +7,6 @@ Page({
     // user:{}
     slides:null,
     entities:null,
-    
-      pic:'https://cdn.ferrari.com/cms/network/media/img/resize/649ec85da3933d0095d1203f-ferrari-models-menu-2023-cards-sf90-stradale?width=800&height=450'
-    
   },
   // 生命周期 
   onLoad(){
