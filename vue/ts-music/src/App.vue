6bb378e1867a87b6f3ddae0d4007acb238e36f4f
YangyangU <495541711@qq.com>
2023-12-23 12:05:44 +0800

    'ts-music'

---
+++ vue/ts-music/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onMounted,ref } from 'vue';
+import {useBanner} from './api/index';
+import type { Banner } from './models/banner';
+
+
+let banners = ref<Banner[]>([]) //vue 的响应式
+onMounted(async ()=>{
+  banners.value = await useBanner()
+  console.log(banners.value);
+  
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
