5156b9c1f9ae6b445c3e27a9111dd920415d2758
YangyangU <495541711@qq.com>
2024-01-24 19:22:28 +0800

    'leetcode'

--- leetcode/110/1.js
+++ leetcode/110/1.js
@@ -1,6 +1,13 @@
 
 
-var isBalanced = function(root) {
-    if (!root) return true;
-    
+var isBalanced = function (root) {
+    function getHeight(node) {
+        if (node === null) return 0;
+        const leftH = getHeight(node.left);
+        if (leftH === -1) return -1; // 提前退出，不再递归
+        const rightH = getHeight(node.right);
+        if (rightH === -1 || Math.abs(leftH - rightH) > 1) return -1;
+        return Math.max(leftH, rightH) + 1;
+    }
+    return getHeight(root) !== -1;
 };
