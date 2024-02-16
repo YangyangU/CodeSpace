01812061620afe0b8b8e8f3e7450aaccdff06d94
YangyangU <495541711@qq.com>
2024-02-16 23:56:53 +0800

    'leetcode'

--- leetcode/700/1.js
+++ leetcode/700/1.js
@@ -1,5 +1,5 @@
 
-//递归
+//递归使用
 var searchBST = function(root, val) {
     if (root == null || root.val == val) return root;
     return root.val < val ? searchBST(root.right,val) : searchBST(root.left,val)
@@ -12,3 +12,4 @@ var searchBST = function(root, val) {
     }
     return root
 };
+
