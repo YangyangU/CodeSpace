03c36508cafa7c136d9b436997f479503c056c80
YangyangU <495541711@qq.com>
2024-04-01 13:41:27 +0800

    快排

---
+++ 复习/手写/快排.js
@@ -0,0 +1,20 @@
+let arr = [1,2,7,9,4,3,5,7,9]
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
+}
+
+console.log(quickSort(arr));
