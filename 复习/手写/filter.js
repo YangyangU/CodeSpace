3bbf4249c4f3f1c002620bd730e2e9a5bca9fb33
YangyangU <495541711@qq.com>
2024-04-11 15:59:57 +0800

    手写update

---
+++ 复习/手写/filter.js
@@ -0,0 +1,9 @@
+function myFilter(arr, fn) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i, arr)) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
