a11ce16e33bf7b24f5fba052193c3db4403df459
YangyangU <495541711@qq.com>
2024-04-24 00:42:44 +0800

    react-article

---
+++ React/article/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from './pages/login'
+import Layout from './pages/layout'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className='app'>
+                <Routes>
+                    <Route path='/' element={<Layout />}></Route>
+                    <Route path='/login' element={<Login />}></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default App;
