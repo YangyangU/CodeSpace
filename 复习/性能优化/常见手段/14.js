dfea6e829045b57fc4b5ecf133f0f740692f8847
YangyangU <495541711@qq.com>
2024-04-24 18:54:26 +0800

    性能优化-update

---
+++ 复习/性能优化/常见手段/14.js
@@ -0,0 +1,27 @@
+let color = 'pink'
+
+if(color = 'red'){
+
+}else if(color = 'blue'){
+    
+}else if(color = 'pink'){
+    
+}else if(color = 'yellow'){
+    
+}else if(color = 'brown'){
+    
+}
+
+
+switch(color){
+    case 'red':
+        break;
+    case 'blue':
+        break;
+    case 'pink':
+        break;
+    case 'yellow':
+        break;
+    case 'brown':
+        break;
+}
