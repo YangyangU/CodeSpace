8724b5f14f1b8fb5c76df5b615e1068606823e3a
YangyangU <495541711@qq.com>
2024-05-12 23:49:31 +0800

    复习-update

--- 复习/场景/链表反转.js
+++ 复习/场景/链表反转.js
@@ -1,20 +1,33 @@
-function reverseList(head) {
-    let prev = null;
-    let current = head;
-    while (current != null) {
-        let nextTemp = current.next;//存储下一个节点
-        current.next = prev;
-        prev = current;
-        current = nextTemp;
-    }
-    return prev;
-}
+// function reverseList(head) {
+//     let prev = null;
+//     let current = head;
+//     while (current != null) {
+//         let nextTemp = current.next;//存储下一个节点
+//         current.next = prev;
+//         prev = current;
+//         current = nextTemp;
+//     }
+//     return prev;
+// }
 
 // 测试
 
 
 
 // 创建一个链表：1 -> 2 -> 3 -> 4 -> 5
+
+function reverseList(head){
+    let pre = null
+    let current = head
+    while(current != null){
+        let temp = current.next
+        current.next = pre
+        pre = current
+        current = temp
+    }
+    return pre
+}
+
 const head = {
     value: 1,
     next: {
