5ea415d1ffb6a5e87cc6ef8b6402f8da8a58d7dd
xuyangyang3 <495541711@qq.com>
2024-08-14 17:20:34 +0800

    feat: 看代码说结果

---
+++ 复习/看代码说结果/1.js
@@ -0,0 +1,38 @@
+var a
+var b = new Promise((resolve) => {
+    console.log(1);
+    setTimeout(() => {
+        resolve(2)
+    }, 1000)
+}).then(() => {
+    console.log(3);
+}).then(() => {
+    console.log(4);
+}).then(() => {
+    console.log(5);
+})
+a = new Promise(async (resolve) => {
+    console.log(a)
+    await b
+    console.log(a)
+    await a
+    resolve(true)
+    console.log(6);
+})
+console.log('end');
+
+
+// 1 un 2 3 4 5 pending p 6 end
+
+// a死锁走不出来 不打印6
+
+// a的值从
+
+// 答案
+// 1
+// undefined
+// end
+// 3
+// 4
+// 5
+// Promise { <pending> }
