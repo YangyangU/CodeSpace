343500e462569566f30ac462e1d05ec9a43e9ea4
xuyangyang3 <495541711@qq.com>
2024-08-22 21:37:57 +0800

    feat: 右键菜单init

--- vue/vue-context-menu/src/App.vue
+++ vue/vue-context-menu/src/App.vue
@@ -1,9 +1,43 @@
 <template>
-    <ContextMenu />
+    <ContextMenu :items="items" :visible="visible" />
 </template>
 
 <script setup lang="ts">
 import ContextMenu from './components/ContextMenu.vue';
+import { ref } from 'vue';
+
+interface ItemType {
+    label: string;
+    key: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    children?: ItemType[];
+    type?: 'group';
+}
+
+const items: ItemType[] = [
+    {
+        label: 'Copy',
+        key: 'copy',
+    },
+    {
+        label: 'Cut',
+        key: 'cut',
+    },
+    {
+        label: 'Paste',
+        key: 'paste',
+    },
+    {
+        label: 'Delete',
+        key: 'delete',
+    },
+    {
+        label: 'Undo',
+        key: 'undo',
+    },
+];
+
+const visible = ref<boolean>(false);
 </script>
 
 <style scoped></style>
--- vue/vue-context-menu/src/components/ContextMenu.vue
+++ vue/vue-context-menu/src/components/ContextMenu.vue
@@ -1,7 +1,39 @@
 <template>
-    <div>caidan</div>
+    <a-menu
+        v-if="visible"
+        id="dddddd"
+        v-model:openKeys="openKeys"
+        v-model:selectedKeys="selectedKeys"
+        style="width: 256px"
+        mode="inline"
+        :items="props.items"
+        theme="dark"
+        @click="handleClick"
+    ></a-menu>
 </template>
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
+import type { MenuProps } from 'ant-design-vue';
 
-<script setup lang="ts"></script>
+const selectedKeys = ref<string[]>(['1']);
+const openKeys = ref<string[]>(['sub1']);
 
-<style scoped></style>
+const handleClick: MenuProps['onClick'] = (e) => {
+    console.log('click', e);
+};
+
+watch(openKeys, (val) => {
+    console.log('openKeys', val);
+});
+
+const props = defineProps({
+    items: {
+        type: Array,
+        default: () => [],
+    },
+    visible: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
