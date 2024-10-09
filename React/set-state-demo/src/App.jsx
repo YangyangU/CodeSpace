4f1284a20eddaee59f11dd662b719561ebee9f10
xuyangyang3 <495541711@qq.com>
2024-10-09 15:55:38 +0800

    feat: setState

--- React/set-state-demo/src/App.jsx
+++ React/set-state-demo/src/App.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 class App extends React.Component {
   state = {
@@ -8,54 +7,16 @@ class App extends React.Component {
   };
 
   handleClick = () => {
-    const fans = Math.floor(Math.random() * 10);
-    setTimeout(() => {
-      console.log("宏任务触发");
-      Promise.resolve().then(() => {
-        console.log("微任务触发");
-      });
-      this.setState(
-        {
-          count: this.state.count + fans,
-        },
-        () => {
-          console.log("新增粉丝数:", fans);
-        }
-      );
-    });
+    this.setState({ count: this.state.count + 1 });
+    this.setState({ count: this.state.count + 1 });
   };
-  componentDidMount() {
-    const fans = Math.floor(Math.random() * 10);
-    setTimeout(() => {
-      console.log("宏任务触发");
-    })
-    Promise.resolve().then(() => {
-      console.log("微任务触发");
-    });
-    this.setState(
-      {
-        count: this.state.count + fans,
-      },
-      () => {
-        console.log("新增粉丝数:", fans);
-      }
-    );
-    console.log("tongbu任务触发");
-  }
-
   render() {
     return (
       <div className="App">
-        <img
-          src={reactLogo}
-          alt="logo"
-          className="App-logo"
-          onClick={this.handleClick}
-        />
-        <p>我的关注人数：{this.state.count}</p>
+        <button onClick={this.handleClick}>+1</button>
+        <p>count：{this.state.count}</p>
       </div>
     );
   }
 }
-
-export default App;
+export default App
