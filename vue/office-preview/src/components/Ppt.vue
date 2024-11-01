58498df806277d713f4184a561462bf7055b44f3
徐阳阳-BJS28454 <xuyy14@youdao>
2024-11-01 10:05:43 +0800

    feat: office-preview

---
+++ vue/office-preview/src/components/Ppt.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <VueOfficePdf />
+    </div>
+</template>
+
+<script setup lang="ts">
+import VueOfficePdf from '@vue-office/pdf';
+
+</script>
+
+<style scoped>
+
+</style>
--- vue/office-preview/src/main.ts
+++ vue/office-preview/src/main.ts
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import FileViewer from '@flyfish-group/file-viewer3'
 
-import '@flyfish-group/file-viewer3/dist/style.css'
-
-createApp(App).use(FileViewer).mount('#app')
+createApp(App).mount('#app')
