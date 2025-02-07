4f88d8f94abf9abf4dd1dbfb5333dd6240f9c9c1
YangyangU <495541711@qq.com>
2025-02-07 17:46:37 +0800

    feat: await undefined

--- 面试题/抖音哲玄/看代码说结果.js
+++ 面试题/抖音哲玄/看代码说结果.js
@@ -1,5 +1,5 @@
 /*
- * 题目1
+ * 题目1 —— 微任务爆栈
  * 结果：微任务递归执行，栈溢出再执行宏任务
  */
 setTimeout(() => {
@@ -12,3 +12,26 @@ function test() {
     });
 }
 test();
+
+/*
+ * 题目2 —— await undefined
+ * 结果：1 3 5 4 2
+ * 分析：await undefined相当于await Promise.resolve(undefined)，即await Promise.resolve()
+ */
+
+const b = new Promise((resolve) => {
+    console.log(1);
+    setTimeout(() => {
+        resolve();
+    }, 1000);
+}).then(() => {
+    console.log(2);
+});
+let a;
+a = new Promise(async (resolve) => {
+    console.log(3);
+    await a;
+    resolve(true);
+    console.log(4);
+});
+console.log(5);
